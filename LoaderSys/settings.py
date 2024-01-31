76aaef2f82e934171ef1fc964c1c7b605701b8fc
ellis <mapakameellis@gmail.com>
2024-01-31 18:02:08 +0200

    update database config

--- LoaderSys/settings.py
+++ LoaderSys/settings.py
@@ -91,10 +91,18 @@ WSGI_APPLICATION = 'LoaderSys.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'railway',
+        'USER': 'root',
+        'PASSWORD': 'GdGbbbcCg34hhe11B-5ChE6fbe-e24e3',
+        'HOST': 'monorail.proxy.rlwy.net',   
+        'PORT': '27808',
+}   
 }
 
 
