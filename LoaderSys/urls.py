311eccd8bde314275ea29198733ab701b825b3f9
ellis <mapakameellis@gmail.com>
2024-02-01 19:42:55 +0200

    editing settings and urls

--- LoaderSys/urls.py
+++ LoaderSys/urls.py
@@ -14,7 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path as url
+from django.conf.urls.static import static
+from django.views.static import serve
+from django.conf import settings
 
 urlpatterns = [
     
@@ -25,5 +28,14 @@ urlpatterns = [
     path('analytics/', include('analytics.urls')),
     path('manager/', include('manager.urls')),
     path('users/', include('users.urls')),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+
+
+
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
